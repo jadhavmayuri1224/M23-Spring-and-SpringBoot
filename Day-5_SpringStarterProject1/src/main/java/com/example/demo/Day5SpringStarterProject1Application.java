8d17e212828257a11c4e9e5e253e2f493ce20e3d
Admin <Admin@DESKTOP-1ERB0H2>
2022-09-27 20:32:19 +0530

    First Commit

---
+++ Day-5_SpringStarterProject1/src/main/java/com/example/demo/Day5SpringStarterProject1Application.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Day5SpringStarterProject1Application{
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext c=SpringApplication.run(Day5SpringStarterProject1Application.class, args);
+		Student s=c.getBean(Student.class);
+		s.display();
+	
+	}
+}
